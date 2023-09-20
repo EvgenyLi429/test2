8a3c2674959e5f361f78110a7d6159a18804dec9
Evgeny Likhachev <EvgenyLi429@gmail.com>
2023-09-20 22:54:31 +0200

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Evgeny Likhachev on 20/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
